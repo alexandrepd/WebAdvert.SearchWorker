f3f4f01b118ec01d0bc9c89d6e7806615bbfb249
Alexandre Oliveira <alexandre_pd@hotmail.com>
2023-01-01 14:57:16 +0000

    fisr commit with helloworld

---
+++ WebAdvert.SearchWorker/SearchWorker.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace WebAdvert.SearchWorker
+{
+    public class SearchWorker
+    {
+        public void PopulateElasticSearch(SNSEvent snsEvent, ILambdaContext contex)
+        {
+            foreach (var item in snsEvent.Records)
+            {
+                contex.Logger.LogLine(item.Sns.Message);
+            }
+        }
+
+    }
+}
